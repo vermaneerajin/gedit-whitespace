b83237b5bff2a000b0057d88ae1009c2d62651ac
Neeraj Verma <verma.neeraj.in@gmail.com>
2017-11-19 13:23:37 +0530

    Implement window and view extension points
    Consists only console output, For testing purpose

--- gedit-whitespace/__init__.py
+++ gedit-whitespace/__init__.py
@@ -1,7 +1,7 @@
 from gi.repository import GObject, Gedit
 
-class Whitespace(GObject.Object, Gedit.WindowActivatable):
-    __gtype_name__ = "Gedit whitespace plugin"
+class WhitespaceWindowActivatable(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "WhitespaceWindowActivatable"
 
     window = GObject.property(type=Gedit.Window)
 
@@ -9,10 +9,30 @@ class Whitespace(GObject.Object, Gedit.WindowActivatable):
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        pass
+        print "Plugin created for", self.window
 
     def do_deactivate(self):
-        pass
+        print "Plugin stopped for", self.window
 
     def do_update_state(self):
-        pass
+        # Called whenever the window has been updated (active tab
+        # changed, etc.)
+        print "Plugin update for", self.window
+        
+class WhitespaceViewActivatable(GObject.Object, Gedit.ViewActivatable):
+    __gtype_name__ = "WhitespaceViewActivatable"
+
+    view = GObject.property(type=Gedit.View)
+
+    def __init__(self):
+        GObject.Object.__init__(self)
+
+    def do_activate(self):
+        print "Plugin created for", self.view
+
+    def do_deactivate(self):
+        print "Plugin stopped for", self.view
+
+    def do_update_state(self):
+        # Called whenever the view has been updated
+        print "Plugin update for", self.view
