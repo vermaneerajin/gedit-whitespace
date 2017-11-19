2e0b0026f10e9029c9054eda5e1cb0d416e245d2
Neeraj Verma <verma.neeraj.in@gmail.com>
2017-11-19 13:40:20 +0530

    Fix print syntax

--- gedit-whitespace.py
+++ gedit-whitespace.py
@@ -9,15 +9,15 @@ class WhitespaceWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        print "Plugin created for", self.window
+        print("Plugin created for", self.window)
 
     def do_deactivate(self):
-        print "Plugin stopped for", self.window
+        print("Plugin stopped for", self.window)
 
     def do_update_state(self):
         # Called whenever the window has been updated (active tab
         # changed, etc.)
-        print "Plugin update for", self.window
+        print("Plugin update for", self.window)
         
 class WhitespaceViewActivatable(GObject.Object, Gedit.ViewActivatable):
     __gtype_name__ = "WhitespaceViewActivatable"
@@ -28,11 +28,11 @@ class WhitespaceViewActivatable(GObject.Object, Gedit.ViewActivatable):
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        print "Plugin created for", self.view
+        print("Plugin created for", self.view)
 
     def do_deactivate(self):
-        print "Plugin stopped for", self.view
+        print("Plugin stopped for", self.view)
 
     def do_update_state(self):
         # Called whenever the view has been updated
-        print "Plugin update for", self.view
+        print("Plugin update for", self.view)
